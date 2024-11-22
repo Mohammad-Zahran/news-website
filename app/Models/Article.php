ef93f28aa95eca6ea8dee80341048013fec38fa6
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-23 01:37:26 +0200

    updated the article and News Models

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
+    protected $fillable = ['content','news_id','user_id','attachment_path',];
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+    }
+
+    public function news()
+    {
+
+        return $this->belongsTo(News::class);
+    }
     
+
 }
--- app/Models/News.php
+++ app/Models/News.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    protected $fillable = ['title', 'content', 'age_restriction',];
+    protected $fillable = ['title', 'content', 'age_restriction', 'created_by'];
 
     public function user()
     {
