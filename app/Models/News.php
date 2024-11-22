769ba6ffa2ebd15b02cf3fad3624a65af24ec1a2
Mohammad Zahran <125310252+AbouZik@users.noreply.github.com>
2024-11-23 01:32:38 +0200

    added a function to handle the foreign key in news

--- app/Models/News.php
+++ app/Models/News.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    protected $fillable = ['title','content','age_restriction',];
+    protected $fillable = ['title', 'content', 'age_restriction',];
+
+    public function user()
+    {
+
+        return $this->belongsTo(User::class);
+    }
+    
 }
-
-
